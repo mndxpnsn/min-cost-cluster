bce26d860e7187cee7493ba0349a80aa4d5baeb7
derek <harrison.derek.w@gmail.com>
2022-06-08 12:16:36 +0200

    cut out zeros in min method and fix bottom-up algorithm

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,18 @@ public class Main {
     static double min(double x, double y) {
         double res = 0.0;
 
-        if(x > 0 || y > 0) {
-            if (x < y) res = x;
-            else res = y;
+        // Check for zeros
+        if(x != 0.0 && y == 0.0) {
+            return x;
         }
+
+        if(x == 0.0 && y != 0.0) {
+            return y;
+        }
+
+        // Compute minimum
+        if (x < y) res = x;
+        else res = y;
 
         return res;
     }
@@ -28,20 +36,32 @@ public class Main {
         }
     }
 
+    static void print_mat(double[][] mat, int n) {
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < n; ++j) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
     static double recur(double[][] X, int i, int j, double[][] dp) {
         double res = 0.0;
 
+        // Get results from memo table if available
         if(dp[i][j] != 0.0) {
-            return 0.0;
+            return dp[i][j];
         }
 
+        // Return cluster of one node
         if(i == j - 1) {
             return X[i][j];
         }
 
+        // Compute minimum cluster cost
         if(i < j - 1) {
             double min_val = 0.0;
-            for(int k = i + 1; k < j - 1; ++k) {
+            for(int k = i; k < j; ++k) {
                 double val1 = recur(X, i, k, dp);
                 double val2 = recur(X, k + 1, j, dp);
                 min_val = min(val1 + val2, min_val);
@@ -50,6 +70,7 @@ public class Main {
             res = X[i][j] + min_val;
         }
 
+        // Store results in memo table
         dp[i][j] = res;
 
         return res;
@@ -64,8 +85,8 @@ public class Main {
             for (int i = 0; i < n; ++i) {
                 if(i + j < n) {
                     double min_val = 0.0;
-                    for(int k = i + 1; k < j - 1; ++k) {
-                        min_val = min(L[i][k] + L[k + 1][j], min_val);
+                    for(int k = i; k <= i + j; ++k) {
+                        min_val = min(L[i][k] + L[k + 1][i + j], min_val);
                     }
 
                     L[i][i + j] = X[i][i + j] + min_val;
@@ -91,7 +112,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Size of input vector
-        int n = 5;
+        int n = 10;
 
         // Declare and initialize cost matrix
         double[][] X = new double[n + 1][n + 1];
